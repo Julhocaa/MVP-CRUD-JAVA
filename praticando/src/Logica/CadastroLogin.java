67d545a06d3f76d54a47e71581a9499d5dcfc380
Julio <juliobdc34@gmail.com>
2026-05-11 09:20:48 -0400

    atualizacaoSegunda

--- praticando/src/Logica/CadastroLogin.java
+++ praticando/src/Logica/CadastroLogin.java
@@ -3,6 +3,8 @@ import java.util.*;
 
 public class CadastroLogin {
 
+
+    //cadastro usuario
    public static void main(String[] args) {
        Scanner loginUser = new Scanner(System.in);
        System.out.println("Bem vindo ao Sistemas&Julios insira suas credencias por gentiliza");
@@ -16,9 +18,50 @@ public class CadastroLogin {
        System.out.println("repita sua senha: ");
        String senhaDigitada = loginUser.nextLine();
 
-        MoldeCL cadastrarLogar = new MoldeCL(usuario,senha,senhaDigitada);
+       do{
+           if(!senha.equals(senhaDigitada)) {
+               System.out.println("repita novamento o processo");
+
+               System.out.println("Senha: ");
+               senha = loginUser.nextLine();
+
+               System.out.println("repita sua senha: ");
+               senhaDigitada = loginUser.nextLine();
+           }
+
+           }while(!senha.equals(senhaDigitada));
+
+       MoldeCL cadastrarLogar = new MoldeCL(usuario,senha);
 
 
+       System.out.println("Usuario: ");
+       String usuarioLog = loginUser.nextLine();
+
+       System.out.println("Senha: ");
+       String senhaLog = loginUser.nextLine();
+
+       //login usuario
+
+       while(!usuarioLog.equals(usuario) || !senhaLog.equals(senha)){
+
+           if(cadastrarLogar.login(usuarioLog, senhaLog)){
+
+               System.out.println("Bem vindo de voltar: "+usuarioLog+"!!!!!");
+
+                break;
+           }else {
+               System.out.println("seu usuario ou a senha estão incorretos... tente novamente porfavor");
+
+               System.out.println("Usuario: ");
+               usuarioLog = loginUser.nextLine();
+
+               System.out.println("Senha: ");
+               senhaLog = loginUser.nextLine();
+
+           }
+
+       }
+            System.out.println("Bem vindo de voltar: "+usuarioLog+"!!!!!");
     }
 
 }
--- praticando/src/Logica/MoldeCL.java
+++ praticando/src/Logica/MoldeCL.java
@@ -1,40 +1,24 @@
 package Logica;
-import java.util.*;
-
 public class MoldeCL {
 
     //Atributos
 
     private String usuario;
     private String senha;
-    private String senhaDigitada;
 
-    public MoldeCL(String usuario, String senha, String senhaDigitada){
-        Scanner sc =new Scanner(System.in);
+    public MoldeCL(String usuario, String senha){
 
-        do{
-
-            if(!senha.equals(senhaDigitada)){
-                System.out.println("repita novamento o processo");
-
-                System.out.println("Senha: ");
-                senha = sc.nextLine();
-
-                System.out.println("repita sua senha: ");
-                senhaDigitada = sc.nextLine();
-
-            }
-
-        }while(!senha.equals(senhaDigitada));
 
         this.usuario = usuario;
         this.senha = senha;
 
-        System.out.println("Ola "+usuario +"que bom em ter você conosco!!!");
+        System.out.println("Ola "+usuario +" que bom em ter você conosco!!!");
 
     }
 
-//    public boolean login(String usuarioDigitado, String senhaDigitada){
-//        return this.usuario.equals(usuarioDigitado) && this.senha.equals(senhaDigitada);
-//    }
+    public boolean login(String usuarioDigitado, String senhaDigitada){
+
+        return this.usuario.equals(usuarioDigitado) && this.senha.equals(senhaDigitada);
+
+    }
 }
